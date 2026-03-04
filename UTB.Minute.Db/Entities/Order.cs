e17a092e6ec112394d3dc03762b97b848546aad5
pavel svoboda <pavel97svoboda@seznam.cz>
2026-03-04 15:42:46 +0100

    ClassAdd

---
+++ UTB.Minute.Db/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using UTB.Minute.Contracts;
+
+namespace UTB.Minute.Db.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public MenuEntry MenuEntry { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ UTB.Minute.Db/Entities/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTB.Minute.Db.Entities
+{
+    public enum OrderStatus
+    {
+        Created,
+        Cooking,
+        Ready,
+        Cancelled
+    }
+}
